77342c6eafa7ed9d08451fad76a3a37bb7143678
Duvis07 <96325513+Duvis07@users.noreply.github.com>
2022-06-07 18:37:50 -0500

    "Se actualiza libreria swagger"

--- src/main/java/com/springBajo8/springBajo8/domain/citasDTOReactiva.java
+++ src/main/java/com/springBajo8/springBajo8/domain/citasDTOReactiva.java
@@ -1,12 +1,13 @@
 package com.springBajo8.springBajo8.domain;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.UUID;
-
+@Data
 @Document(collection = "citas")
 public class citasDTOReactiva {
 
@@ -29,77 +30,6 @@ public class citasDTOReactiva {
 
     private String estadoReservaCita;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getIdPaciente() {
-        return idPaciente;
-    }
-
-    public void setIdPaciente(String idPaciente) {
-        this.idPaciente = idPaciente;
-    }
-
-    public String getNombrePaciente() {
-        return nombrePaciente;
-    }
-
-    public void setNombrePaciente(String nombrePaciente) {
-        this.nombrePaciente = nombrePaciente;
-    }
-
-    public String getApellidosPaciente() {
-        return apellidosPaciente;
-    }
-
-    public void setApellidosPaciente(String apellidosPaciente) {
-        this.apellidosPaciente = apellidosPaciente;
-    }
-
-    public String getNombreMedico() {
-        return nombreMedico;
-    }
-
-    public void setNombreMedico(String nombreMedico) {
-        this.nombreMedico = nombreMedico;
-    }
-
-    public String getApellidosMedico() {
-        return apellidosMedico;
-    }
-
-    public void setApellidosMedico(String apellidosMedico) {
-        this.apellidosMedico = apellidosMedico;
-    }
-
-    public LocalDate getFechaReservaCita() {
-        return fechaReservaCita;
-    }
-
-    public void setFechaReservaCita(LocalDate fechaReservaCita) {
-        this.fechaReservaCita = fechaReservaCita;
-    }
-
-    public String getHoraReservaCita() {
-        return horaReservaCita;
-    }
-
-    public void setHoraReservaCita(String horaReservaCita) {
-        this.horaReservaCita = horaReservaCita;
-    }
-
-    public String getEstadoReservaCita() {
-        return estadoReservaCita;
-    }
-
-    public void setEstadoReservaCita(String estadoReservaCita) {
-        this.estadoReservaCita = estadoReservaCita;
-    }
 
     //private Date sendedDate = new Date();
 
